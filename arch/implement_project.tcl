d07ab6c54cd7cbb6c401900209edc18079358972
Russell Joyce <russell.joyce@york.ac.uk>
2017-12-21 15:21:05 +0000

    Fixed implementation script returning before outputs were generated
    Launching synthesis/implementation runs in Vivado can happen asynchronously,
    so the script must explicitly wait for these to complete before continuing.

--- arch/implement_project.tcl
+++ arch/implement_project.tcl
@@ -1,3 +1,7 @@
 open_project ../hwproj/hwproj.xpr
 update_compile_order -fileset sources_1
 launch_runs impl_1 -to_step write_bitstream -jobs 2
+wait_on_run impl_1
+if {[get_property PROGRESS [get_run impl_1]] != "100%"} {
+	error "ERROR: impl_1 failed"
+}
