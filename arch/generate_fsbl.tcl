92d8fa388e90fcc7efbea70f11e8ac3f773d56c2
Russell Joyce <russell.joyce@york.ac.uk>
2018-04-18 21:53:20 +0100

    Updated FSBL generation to work with newer versions of Vivado
    It seems that 2017.4 puts the .hwdef file in a different place. This solution
    probably isn't the most robust and future-proof, but it works fine for now.

--- arch/generate_fsbl.tcl
+++ arch/generate_fsbl.tcl
@@ -1,3 +1,9 @@
-set hwdsgn [open_hw_design ../hwproj/hwproj.srcs/sources_1/bd/design_1/hdl/design_1.hwdef]
+# Hardware definition file can be in one of two places, depending on Vivado version
+set filename "../hwproj/hwproj.srcs/sources_1/bd/design_1/hdl/design_1.hwdef"
+if {![file exist $filename]} {
+	set filename "../hwproj/hwproj.srcs/sources_1/bd/design_1/synth/design_1.hwdef"
+}
+
+set hwdsgn [open_hw_design $filename]
 generate_app -hw $hwdsgn -os standalone -proc ps7_cortexa9_0 -app zynq_fsbl -compile -sw fsbl -dir ../fsbl
 exit
